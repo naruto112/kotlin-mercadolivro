641e1e3cfa2afb78e5f2e7e396749840bc9ead62
Renato Rock <renatorock3@hotmail.com>
2022-02-01 00:08:01 -0300

    First code

---
+++ src/main/kotlin/com/mercadolivro/service/BookService.kt
@@ -0,0 +1,51 @@
+package com.mercadolivro.service
+
+import com.mercadolivro.enums.BookStatus
+import com.mercadolivro.model.BookModel
+import com.mercadolivro.model.CustomerModel
+import com.mercadolivro.repository.BookrRepository
+import org.springframework.data.domain.Page
+import org.springframework.stereotype.Service
+import org.springframework.data.domain.Pageable
+
+@Service
+class BookService(
+    val bookRepository: BookrRepository
+) {
+    fun create(book: BookModel) {
+        bookRepository.save(book)
+    }
+
+    fun findAll(pageable: Pageable): Page<BookModel> {
+         return bookRepository.findAll(pageable)
+    }
+
+    fun findActives(pageable: Pageable): Page<BookModel> {
+        return bookRepository.findByStatus(BookStatus.ATIVO, pageable)
+    }
+
+    fun findById(id: Int): BookModel {
+        return bookRepository.findById(id).orElseThrow{ Exception("Não existe este recurso")}
+    }
+
+    fun delete(id: Int) {
+        val book = findById(id)
+
+        book.status = BookStatus.CANCELADO
+
+        update(book)
+    }
+
+    fun update(book: BookModel) {
+        bookRepository.save(book)
+    }
+
+    fun deleteByCustomer(customer: CustomerModel) {
+        val books = bookRepository.findByCustomer(customer)
+        for(book in books) {
+            book.status = BookStatus.DELETADO
+        }
+        bookRepository.saveAll(books)
+    }
+
+}
